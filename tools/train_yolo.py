5a18b622edf421f3d415c7c74b3ff3ae992638bc
Zirui_Li <le13851690127@gmail.com>
2024-04-16 00:44:26 -0400

    updates

--- tools/train_yolo.py
+++ tools/train_yolo.py
@@ -7,17 +7,17 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     model = YOLO('yolov8x.yaml')
-    # model = model.load(os.path.join(cfgs.CHECKPOINTS.root, 'yolov8x.pt'))
-    model = model.load('D:\Machine_Learning_Projects\HE2L-Net\outputs\yolo_runs\\train5\weights\\best.pt')
+    model = model.load(os.path.join(cfgs.CHECKPOINTS.root, 'yolov8x.pt'))
+    # model = model.load('/home/zli133/ECE_208/HE2L-Net/outputs/yolo_runs/train5/weights/last.pt')
     save_dir = os.path.join(cfgs.OUTPUTS.root, 'yolo_runs')
     results = model.train(data=cfgs.TRAIN_Rec.DATASET.config, epochs=cfgs.TRAIN_Rec.epochs, imgsz=cfgs.TRAIN_Rec.image_size, project=save_dir, device=0)
 
     image_names = range(10)
-    image_root = 'D:\Machine_Learning_Projects\HE2L-Net\datasets\kaggle_data_coco\images'
+    image_root = cfgs.TRAIN_Rec.DATASET.train_img
     image_path = [os.path.join(image_root, str(image_name)+'.jpg') for image_name in image_names]
     pred_result = model(image_path)
     # Visualization
-    for result in pred_result:
+    for i, result in enumerate(pred_result):
         rgb_img = cv2.cvtColor(result.orig_img, cv2.COLOR_BGR2RGB)
         plt.imshow(rgb_img)
         plt.axis('off')
@@ -30,4 +30,6 @@ if __name__ == '__main__':
             plt.gca().add_patch(rect)
             plt.text(x1, y1, f'{result.names[int(cls)]}: {conf:.2f}', color='white', fontsize=12,
                      bbox=dict(facecolor='red', alpha=0.5))
+        plt.savefig(os.path.join(save_dir, f'visualized{i}.png'))
         plt.show()
+        plt.close()
